5f1d60c5f591a14e46c0db4002f93c8a75fdfc03
ghori <ghori@DESKTOP-60KQHIC>
2022-07-11 22:03:03 +0530

    Add project files.

---
+++ Assignment/Player.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Player
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("role")]
+        public string Role { get; set; }
+
+        [JsonProperty("price-in-crores")]
+        public string PriceInCrores { get; set; }
+    }
+}
---
+++ Assignment/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Assignment
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string jsonString = File.ReadAllText("json.txt");
+
+        [TestMethod]
+        public void ForeignPlayerCount()
+        {
+            var team = JsonConvert.DeserializeObject<Team>(jsonString);
+            var foreignePlayerCount = team.Player.FindAll(x => x.Country != "India").Count;
+            Assert.IsTrue(foreignePlayerCount == 4, $"Team does not have 4 foreign palyers. Actual: {foreignePlayerCount}");
+        }
+
+
+        [TestMethod]
+        public void WicketKeeperCount()
+        {
+            var team = JsonConvert.DeserializeObject<Team>(jsonString);
+            var wicketKeeperCount = team.Player.FindAll(x => x.Role == "Wicket-keeper").Count;
+            Assert.IsTrue(wicketKeeperCount >= 1, $"Team has less than 1 Wicket-keeper. Actual: {wicketKeeperCount}");
+        }
+    }
+}
